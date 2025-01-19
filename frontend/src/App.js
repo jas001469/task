6bd538dc59b0dabd032b9d91908363ac9437866c
Jas Singh Ajmani <jas001469@gmail.com>
2025-01-19 16:10:06 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import UserForm from "./components/UserForm";
 import AdminDashboard from "./components/AdminDashboard";
 
